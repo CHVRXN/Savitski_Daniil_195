361c435eaf87e02b9b9e4be42b9982718d51f4d3
CHVRXN <90219892+CHVRXN@users.noreply.github.com>
2021-09-26 16:04:04 +0300

    Add files via upload

---
+++ UnitTest/classException/C#/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exception1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, result;
+            try
+            {
+                Console.Write("Первое число: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Второе число: ");
+                b = Convert.ToDouble(Console.ReadLine());
+                if (b == 0) throw new DivideByZeroException();
+                Console.WriteLine("Результат: " + (result = a / b));
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Деление на 0 невозможно");
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверное значение");
+            }
+            catch
+            {
+                Console.WriteLine("Неизвестная ошибка операции");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Размер типа данных int = " + sizeof(int) + " bytes");
+            Console.WriteLine("Размер типа данных float = " + sizeof(float) + " bytes");
+            Console.ReadKey();
+        }
+    }
+}
