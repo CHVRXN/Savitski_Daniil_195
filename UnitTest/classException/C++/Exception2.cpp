523c956010040d6a385f374f2e5b11e9dd0aa3b4
CHVRXN <90219892+CHVRXN@users.noreply.github.com>
2021-09-26 16:04:55 +0300

    Add files via upload

---
+++ UnitTest/classException/C++/Exception2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double a, b, result;
+
+    try
+    {
+        cout << "Первое число: ";
+        cin >> a;
+        cout << "Второе число: ";
+        cin >> b;
+        if (b == 0) {
+            throw "Деление на ноль невозможно\n";
+        }
+        else throw a, b;
+    }
+    catch (const char* exception)
+    {
+        cerr << "Ошибка: " << exception << endl;
+    }
+    catch (double dbeer)
+    {
+        cerr << "\nНеверное значение" << endl;
+    }
+    catch (exception& e)
+    {
+        cout << e.what();
+    }
+    catch (invalid_argument)
+    {
+        cerr << "ошибка операции 0" << endl;
+    }
+    cout << "Размер типа данных int = " << sizeof(int) << " bytes" << endl;
+    cout << "Размер типа данных float = " << sizeof(float) << " bytes" << endl;
+    cout << "Размер типа данных char = " << sizeof(char) << " bytes" << endl;
+
+    return 0;
+}
